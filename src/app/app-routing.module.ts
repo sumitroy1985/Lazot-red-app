d9391e3d53181f1bea7426f4079739ac24c84510
sumitroy <sumit@logic-square.com>
2021-03-12 17:36:56 +0530

    first upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,56 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'splash-screen',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'new-user-register',
+    loadChildren: () => import('./new-user-register/new-user-register.module').then( m => m.NewUserRegisterPageModule)
+  },
+  {
+    path: 'gateway',
+    loadChildren: () => import('./gateway/gateway.module').then( m => m.GatewayPageModule)
+  },
+  {
+    path: 'pattern-login',
+    loadChildren: () => import('./pattern-login/pattern-login.module').then( m => m.PatternLoginPageModule)
+  },
+  {
+    path: 'gateway-onboarding',
+    loadChildren: () => import('./gateway-onboarding/gateway-onboarding.module').then( m => m.GatewayOnboardingPageModule)
+  },
+  {
+    path: 'gateway-configuration',
+    loadChildren: () => import('./gateway-configuration/gateway-configuration.module').then( m => m.GatewayConfigurationPageModule)
+  },
+  {
+    path: 'add-device',
+    loadChildren: () => import('./add-device/add-device.module').then( m => m.AddDevicePageModule)
+  },
+  {
+    path: 'scan-now',
+    loadChildren: () => import('./scan-now/scan-now.module').then( m => m.ScanNowPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'splash-screen',
+    loadChildren: () => import('./splash-screen/splash-screen.module').then( m => m.SplashScreenPageModule)
   }
 ];
 
